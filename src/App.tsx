9d1111756ac6ae63e2fd05409adb44413d061e6d
Harsh Singh <hisingh1@gmail.com>
2024-12-06 16:26:27 -0600

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,37 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navigation } from './components/Navigation'
 import { Footer } from './components/Footer'
 import { HomePage } from './pages/HomePage'
-import  {AboutPage}  from './pages/AboutPage'
+import { AboutPage } from './pages/AboutPage'
 import { PerformancesPage } from './pages/PerformancesPage'
 import { ContactPage } from './pages/ContactPage'
+import { useGoogleAnalytics } from './hooks/useGoogleAnalytics'
+
+function AppContent() {
+  useGoogleAnalytics();
+  
+  return (
+    <div className="min-h-screen bg-black flex flex-col">
+      <Navigation />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/performances" element={<PerformancesPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="https://school.learnzouk.com/" />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-black flex flex-col">
-        <Navigation />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/performances" element={<PerformancesPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="https://school.learnzouk.com/" />
-            <Route path="/contact" element={<ContactPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
-  )
+  );
 }
 
 export default App
-
